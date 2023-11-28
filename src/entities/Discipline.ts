ab38ac6960403b8aedae99caba423a7951ff7bf2
Edson Suraty <edinhosuraty@gmail.com>
2023-11-28 18:49:11 -0300

    adiciona getters na classe Disciplina

--- src/entities/Discipline.ts
+++ src/entities/Discipline.ts
@@ -1,12 +1,24 @@
 export class Discipline {
-    disciplineName: string;
-    workload: number;
-    grade: number;
-  
-    constructor(disciplineName: string, workload: number) {
-      this.disciplineName = disciplineName;
-      this.workload = workload;
-      this.grade = 0;
+
+  private _disciplineName: string;
+  private _workload: number;
+  private _grade: number;
+
+  constructor(disciplineName: string, workload: number) {
+    this._disciplineName = disciplineName;
+    this._workload = workload;
+    this._grade = 0;
+  }
+
+  get name(): string {
+    return this._disciplineName;
+  }
+
+  get workload(): number {
+    return this._workload;
+  }
+
+  get grade(): number {
+    return this._grade;
   }
 }
-  
