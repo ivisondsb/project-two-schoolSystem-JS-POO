9a9cc65ad77c91c80049f642c3cdcfb3cb20ac8b
Suamy <suamyvc@hotmail.com>
2023-11-27 23:13:35 -0300

    Criar Classe Curso, Cadastrar Curso, Remover Disciplina, Consultar Disciplina e Remover Curso, interface

---
+++ src/interfaces/Students.ts
@@ -0,0 +1,25 @@
+import { Student } from './Student';
+import { Discipline } from './Discipline';
+
+export interface StudentProps {
+     name: string;
+     shift: string;
+     disciplines: Discipline;
+     students: Student;
+     enrollment: number;
+}
+
+export interface StudentsMethodsProps {
+    addDiscipline(discipline: Discipline): void;
+    removeDiscipline(disciplineName: string): void;
+    listDisciplines(): void;
+    addStudent(student: Student): void;
+    removeStudent(studentId: string): void;
+    listStudents(): void;
+    getName(): string;
+    getShift(): string;
+    getEnrollment(): number;
+}
+
+
+
