3e5c9dbe81f41d12efc746b72e2a3cda78781cc6
Ivisondsb <Ivisonk0@gmail.com>
2023-11-29 20:55:41 -0300

    fix: fixing function errors and refactoring the class

--- src/entities/Student.ts
+++ src/entities/Student.ts
@@ -1,4 +1,4 @@
-import * as promptSync from 'prompt-sync';
+import * as promptSync from "prompt-sync";
 import { StudentProps, StudentsMethodsProps } from "../interfaces/Students";
 import { Course } from "./Course";
 
@@ -9,9 +9,9 @@ export class Student implements StudentsMethodsProps {
   private age: StudentProps["age"];
   private course: StudentProps["course"];
   private id: StudentProps["id"];
-  private students: StudentProps['students'] = [];
+  private students: StudentProps["students"] = [];
 
-  private static studentInstance: Course = new Course('', '');
+  private static studentInstance: Course = new Course("", "");
 
   constructor(name: string, age: number, course: Course, id: number) {
     this.name = name;
@@ -47,23 +47,23 @@ export class Student implements StudentsMethodsProps {
   }
 
   public registerStudent(name: string, age: number): void {
-    try{
+    try {
       if (!this.isOnlyLetters(name)) {
-        throw new Error('Nome inválido. Por favor, insira apenas letras.');
+        throw new Error("Nome inválido. Por favor, insira apenas letras.");
       }
 
       if (isNaN(Number(age))) {
-        throw new Error('Idade inválida. Por favor, insira apenas números.');
+        throw new Error("Idade inválida. Por favor, insira apenas números.");
       }
       console.log("Cursos Disponíveis:");
       for (let i = 0; i < Course.courses.length; i++) {
         console.log(`${i + 1}. ${Course.courses[i].name}`);
       }
-      
+
       const courseInput = Number(prompt("Escolha o número do curso: "));
 
       if (isNaN(Number(courseInput))) {
-        throw new Error('Curso inválido. Por favor, insira apenas números.');
+        throw new Error("Curso inválido. Por favor, insira apenas números.");
       }
 
       const courseIndex = courseInput - 1;
@@ -75,7 +75,7 @@ export class Student implements StudentsMethodsProps {
       let newStudentId = this.students.length + 1;
       const newStudent = new Student(name, age, selectedCourse, newStudentId);
       this.students.push(newStudent);
-      
+
       console.log("Aluno cadastrado com sucesso!");
     } catch (error: any) {
       console.log(error.message);
@@ -85,16 +85,20 @@ export class Student implements StudentsMethodsProps {
   public removeStudent() {
     try {
       if (this.students.length === 0) {
-        throw new Error("Não existem estudantes cadastrados");
+        console.log("Não existem estudantes cadastrados");
+      }
+      console.log("\n----------LISTA DE ALUNOS----------\n");
+      for (let i = 0; i < this.students.length; i++) {
+        console.log(`${this.students[i].id}. ${this.students[i].name}`);
       }
       const studentId = Number(prompt("Informe o id do aluno: "));
       for (let i = 0; i < this.students.length; i++) {
         if (this.students[i].id === studentId) {
           this.students.splice(i, 1);
-          return "Aluno removido com sucesso.";
+          console.log("Aluno removido com sucesso.");
         }
       }
-      return "Aluno não encontrado.";
+      return console.log("Aluno não encontrado.");
     } catch (error: any) {
       console.log(error.message);
     }
@@ -103,15 +107,27 @@ export class Student implements StudentsMethodsProps {
   public checkStudent() {
     try {
       if (this.students.length === 0) {
-        throw new Error("Não existem estudantes cadastrados");
+        console.log("Não existem estudantes cadastrados");
       }
-      const studentId = Number(prompt("Informe o id do aluno: "));
+      console.log("\n----------LISTA DE ALUNOS----------\n");
       for (let i = 0; i < this.students.length; i++) {
-        if (this.students[i].id === studentId) {
-          return this.students[studentId].showData();
+        console.log(`${this.students[i].id}. ${this.students[i].name}`);
+      }
+      const studentId = prompt("Informe o id do aluno: ");
+      for (let i = 0; i < this.students.length; i++) {
+        if (this.students[i].id == Number(studentId)) {
+          const student = this.students[i];
+          const disciplinesName = student.course.disciplines.map((discipline) =>
+            discipline.getName()
+          );
+          const disciplineArray = disciplinesName.join(", ");
+
+          console.log(
+            `Nome: ${student.name}\nIdade: ${student.age}\nCurso: ${student.course.name}\nTurno: ${student.course.shift}\nDisciplinas: ${disciplineArray}`
+          );
         }
       }
-      return "Aluno não encontrado";
+      return console.log("Aluno não encontrado");
     } catch (error: any) {
       console.log(error.message);
     }
@@ -120,7 +136,11 @@ export class Student implements StudentsMethodsProps {
   public updateStudent() {
     try {
       if (this.students.length === 0) {
-        throw new Error("Não existem estudantes cadastrados");
+        console.log("Não existem estudantes cadastrados");
+      }
+      console.log("\n----------LISTA DE ALUNOS----------\n");
+      for (let i = 0; i < this.students.length; i++) {
+        console.log(`${this.students[i].id}. ${this.students[i].name}`);
       }
       const studentId = Number(prompt("Informe o id do aluno: "));
       for (let i = 0; i < this.students.length; i++) {
@@ -130,18 +150,19 @@ export class Student implements StudentsMethodsProps {
           const newAge = Number(
             prompt("Insira a idade do aluno: ") || student.age
           );
-          const newCourse = String(
-            prompt("Insira o curso do aluno: ") || student.course.name
+
+          const newShift = String(
+            prompt("Insira o turno do aluno: ") || student.course.shift
           );
 
           this.students[i].name = newName;
           this.students[i].age = newAge;
-          this.students[i].course.name = newCourse;
+          this.students[i].course.shift = newShift;
 
-          return "As informações do aluno foram atualizadas.";
+          console.log("As informações do aluno foram atualizadas.");
         }
       }
-      return "Aluno não encontrado.";
+      return console.log("Aluno não encontrado.");
     } catch (error: any) {
       console.log(error.message);
     }
@@ -150,7 +171,4 @@ export class Student implements StudentsMethodsProps {
   private isOnlyLetters(input: string): boolean {
     return /^[a-zA-Z\s]+$/.test(input);
   }
-
 }
-
-
