42a456d83bb496d36c7c3555597a46dbd0717a16
Edson Suraty <edinhosuraty@gmail.com>
2023-11-29 19:38:11 -0300

    importando interface Discipline

--- src/interfaces/Discipline.ts
+++ src/interfaces/Discipline.ts
@@ -1,5 +1,12 @@
-interface disciplineProps{
-    _disciplineName: string;
-    _workload: number;
-    _grade: number;
+export interface disciplineProps{
+    disciplineName: string;
+    workload: number;
+    grade: number;
+}
+
+export interface disciplineMethodsProps{
+    getName(): string;
+    getWorkload(): number;
+    getGrade(): number;
+    setName(newName: string): string;
 }
