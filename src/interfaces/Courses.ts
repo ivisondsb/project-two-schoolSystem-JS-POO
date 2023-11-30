81aad060c80d9341b2400440c8b4f1fa453c0d17
Ivisondsb <Ivisonk0@gmail.com>
2023-11-29 21:34:53 -0300

    fix: Adjusting course

--- src/interfaces/Courses.ts
+++ src/interfaces/Courses.ts
@@ -1,20 +1,21 @@
-import { Discipline } from '../entities/Discipline';
-import { Course } from '../entities/Course';
+import { Discipline } from "../entities/Discipline";
+import { Course } from "../entities/Course";
 
 export interface CourseProps {
-    name: string;
-    shift: string;
-    disciplines: Discipline[];
-    courses: Course[];
+  name: string;
+  shift: string;
+  disciplines: Discipline[];
+  courses: Course[];
+  id: number;
 }
 
 export interface CoursesMethodsProps {
-    addCourse(name: string, shift: string): void;
-    removeCourse(name: string): void;
-    listCourses(): void;
-    updateCourse(name: string): void;
-    removeDisciplineByIndex(): void;
-    consultDisciplines(): void;
-    addDisciplineToCourseManually(): void;
-    addDisciplineToCourse(course: Course): void;
+  addCourse(name: string, shift: string): void;
+  removeCourse(): void;
+  listCourses(): void;
+  updateCourse(name: string): void;
+  removeDisciplineByIndex(): void;
+  consultDisciplines(): void;
+  addDisciplineToCourseManually(): void;
+  addDisciplineToCourse(course: Course): void;
 }
