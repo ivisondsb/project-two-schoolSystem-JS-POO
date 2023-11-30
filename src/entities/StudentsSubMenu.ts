06b8ade9eb0635e924dbc8ed84c9af17d95759d4
Ivisondsb <Ivisonk0@gmail.com>
2023-11-29 21:11:48 -0300

    fix: correcting the student submenu

--- src/entities/StudentsSubMenu.ts
+++ src/entities/StudentsSubMenu.ts
@@ -1,41 +1,49 @@
-import * as promptSync from 'prompt-sync';
+import * as promptSync from "prompt-sync";
 
-import { Student } from './Student';
-import { Course } from './Course';
+import { Student } from "./Student";
+import { Course } from "./Course";
 
 const prompt = promptSync();
 
 export class StudentsSubMenu {
-  private static studentInstance: Student = new Student('', 0, new Course('', ''), 0);
+  private static studentInstance: Student = new Student(
+    "",
+    0,
+    new Course("", "", 0),
+    0
+  );
 
   public static start(): void {
     while (true) {
-      console.log('\n=== Gerenciar Alunos ===');
-      console.log('1. Cadastrar Aluno');
-      console.log('2. Consultar Aluno');
-      console.log('3. Remover Aluno');
-      console.log('4. Atualizar Aluno');
-      console.log('5. Voltar ao Menu Principal');
+      console.log("\n=== Gerenciar Alunos ===");
+      console.log("1. Cadastrar Aluno");
+      console.log("2. Consultar Aluno");
+      console.log("3. Remover Aluno");
+      console.log("4. Atualizar Aluno");
+      console.log("5. Voltar ao Menu Principal");
 
-      const option = prompt('Escolha uma opção: ');
+      const option = prompt("Escolha uma opção: ");
 
       switch (option) {
-        case '1':
-          StudentsSubMenu.studentInstance.registerStudent(prompt('Digite o nome do aluno: '), Number(prompt('Digite a idade do aluno: ')));
+        case "1":
+          StudentsSubMenu.studentInstance.registerStudent(
+            prompt("Digite o nome do aluno: "),
+            Number(prompt("Digite a idade do aluno: "))
+          );
           break;
-        case '2':
+        case "2":
           StudentsSubMenu.studentInstance.checkStudent();
           break;
-        case '3':
+        case "3":
           StudentsSubMenu.studentInstance.removeStudent();
           break;
-        case '4':
+        case "4":
           StudentsSubMenu.studentInstance.updateStudent();
           break;
-        case '5':
+        case "5":
           return;
         default:
-          console.log('Opção inválida.');
+          console.log("Opção inválida.");
       }
     }
   }
