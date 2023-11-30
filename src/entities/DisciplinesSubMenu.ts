d58975f54fbfbf410df5457950500ff134f1c206
Ivisondsb <Ivisonk0@gmail.com>
2023-11-29 21:36:55 -0300

    fix: Adjust the subject submenu

--- src/entities/DisciplinesSubMenu.ts
+++ src/entities/DisciplinesSubMenu.ts
@@ -1,41 +1,40 @@
-import * as promptSync from 'prompt-sync'
-import { Course } from './Course';
+import * as promptSync from "prompt-sync";
+import { Course } from "./Course";
 
-const prompt = promptSync()
+const prompt = promptSync();
 
 export class DisciplinesSubMenu {
-  private static courseInstance: Course = new Course('', '');
+  private static courseInstance: Course = new Course("", "", 0);
 
   static start(): void {
     while (true) {
-      console.log('\n=== Gerenciar Disciplinas ===');
-      console.log('1. Cadastrar Disciplina');
-      console.log('2. Consultar Disciplina');
-      console.log('3. Remover Disciplina');
-      console.log('4. Atualizar Disciplina');
-      console.log('5. Voltar ao Menu Principal');
+      console.log("\n=== Gerenciar Disciplinas ===");
+      console.log("1. Cadastrar Disciplina");
+      console.log("2. Consultar Disciplina");
+      console.log("3. Remover Disciplina");
+      console.log("4. Atualizar Disciplina");
+      console.log("5. Voltar ao Menu Principal");
 
-      const option = prompt('Escolha uma opção: ');
+      const option = prompt("Escolha uma opção: ");
 
       switch (option) {
-        case '1':
+        case "1":
           DisciplinesSubMenu.courseInstance.addDisciplineToCourseManually();
           break;
-        case '2':
+        case "2":
           DisciplinesSubMenu.courseInstance.consultDisciplines();
           break;
-        case '3':
+        case "3":
           DisciplinesSubMenu.courseInstance.removeDisciplineByIndex();
           break;
-        case '4':
-          console.log('update discipline');
+        case "4":
+          console.log("update discipline");
           break;
-        case '5':
+        case "5":
           return;
         default:
-          console.log('Opção inválida.');
+          console.log("Opção inválida.");
       }
     }
   }
-
 }
